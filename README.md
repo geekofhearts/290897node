86771fb3368f045667d30c4d44b5eddbdcf6cfdf
Joe Jordan <joe.jordan86@gmail.com>
2011-08-07 23:16:42 +0100

    first commit - a little code.

---
+++ README.md
@@ -0,0 +1,8 @@
+GENODE - Genetic Programing in Node.js
+===
+
+not so much with a plan yet, mostly just some methods for evaling and fitness-testing a bunch of functions-as-strings.
+
+also working on random code generation, and eventually will obviously need mutation / breeding.
+
+test function will be sqrt(x), although will need a more complex one later to test the arbitrary numbers of arguments etc.
---
+++ src/genetic.js
@@ -0,0 +1,92 @@
+
+var variable_name_offset = 96;
+
+function evaluate_function() {
+    var function_as_string = arguments[0];
+    var offset = 96;
+    var index;
+    for (index = 1; index < arguments.length; index++) {
+        eval("var " + String.fromCharCode(variable_name_offset + index) + " = " + JSON.stringify(arguments[index]) + ";");
+    }
+    
+    var ret;
+    eval(function_as_string);
+    
+    return ret;
+}
+
+// Note that the args_array will be referred to in function_as_string as a, b, c...
+function invoke_with_args(function_as_string, args_array) {
+    var eval_args = [function_as_string];
+    // extend the array to include the args_array vars
+    eval_args.push.apply(eval_args, args_array);
+    return evaluate_function.apply(evaluate_function, eval_args);
+}
+
+function test_individual(individual, test_cases) {
+    var results = [];
+    var i;
+    for (i = 0; i < test_cases.length; i++) {
+        results.push(invoke_with_args(individual['routine'], test_cases[i]['arguments']));
+    }
+    return results;
+}
+
+function calculate_combined_fitness_function_for_individual(individual, test_cases, results) {
+    var valid = true;
+    var mean = 0.0, i;
+    for (i = 0; i < test_cases.length; i++) {
+        valid |= test_cases[i]['validate'](results[i]);
+        if (!valid) {
+            break;
+        }
+        Var test_fitness = test_cases[i]['fitness_function'](results[i]);
+        mean = (mean * (i - 1) + test_fitness) / i;
+    }
+    individual['fitness'] = acc;
+    individual['validates'] = valid;
+}
+
+function test_population(population, test_cases) {
+    var results = [];
+    var i;
+    for (i = 0; i < population.length; i++) {
+        results.push(test_individual(population[i], test_cases));
+        calculate_conbined_fitness_function_for_individual(population[i], test_cases, results[i]);
+    }
+
+}
+
+function refresh_routine_from_tokenised_repr(individual, pretty) {
+    var routine = "";
+    var i;
+    for (i = 0; i < individual["code"]["local_vars"].length; i++) {
+        var local_var = individual["code"]["local_vars"][i];
+        routine += "var " + local_var["symbol"] + "=" + JSON.stringify(local_var["initial_value"]) + ";";
+        if (pretty) {
+            routine += "\n";
+        }
+    }
+
+
+
+    individual["routine"] = routine;
+}
+
+function generate_new_individual(number_of_arguments, allowed_operators, return_template) {
+    var new_individual = {};
+    var predefined_vars = [];
+    var local_vars = [];
+    var i;
+    for (i = 1; i <= number_of_arguments; i++) {
+        predefined_vars.push(String.fromCharCode(variable_name_offset + i));
+    }
+    var number_of_local_vars = Math.random() * number_of_arguments * 1.2;
+    for (i = 1; i <= number_of_local_vars; i++) {
+        var varname = number_of_arguments + number_of_local_vars <= 26 ?
+            String.fromCharCode(variable_name_offset + number_of_arguments + i) : 
+            'l' + String.fromCharCode(variable_name_offset + i);
+        local_vars.push(varname);
+    }
+    
+}
